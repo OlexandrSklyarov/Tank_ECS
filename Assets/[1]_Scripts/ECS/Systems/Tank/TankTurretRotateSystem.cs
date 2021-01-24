f922ef4a29a8fb8b13b056bc640e0295d570515c
OlexandrSklyarov <alexskrat@gmail.com>
2021-01-24 18:46:07 +0200

    The barrel rotation process has been implemented. The light is baked in the test scene.

--- Assets/[1]_Scripts/ECS/Systems/Tank/TankTurretRotateSystem.cs
+++ Assets/[1]_Scripts/ECS/Systems/Tank/TankTurretRotateSystem.cs
@@ -9,7 +9,9 @@ namespace SA.Tanks
 
         readonly EcsFilter<TankTurretComponent, MoveComponent, AimingComponent> turretFilter;
 
-        const float MAX_DISTANCE_TO_TARGET = 4f;
+        const float MAX_DIST_TO_TARGET = 4f;
+        const float MAX_BARREL_ROTATE = 35f;
+        
 
         #endregion
 
@@ -26,29 +28,51 @@ namespace SA.Tanks
 
                 //назначаем поворот по умолчанию, как у танка
                 var newTurretRotation = tankRB.rotation;
+                var newBarrelRotation = tankRB.rotation;                
+                var speed = turret.RotateSpeed * Time.deltaTime; 
 
                 //получаем поворот к цели, если она есть и она не слишком близко
                 if (aiming.IsTargetExist &&
-                    Vector3.Distance(turret.Target, turret.TurretTransform.position) > MAX_DISTANCE_TO_TARGET)
+                    Vector3.Distance(turret.Target, turret.TurretTransform.position) > MAX_DIST_TO_TARGET)
                 {
                     //поворачиваем башню вокруг вертикальной оси танка (локально) 
-                    var direction = aiming.AimPosition - turret.TurretTransform.position;
-                    TurretRotation(direction, ref newTurretRotation);                                 
-                }                               
+                    
+                    TurretRotation(aiming.AimPosition, turret, ref newTurretRotation);  
+                    turret.TurretTransform.rotation = Quaternion.Lerp(turret.TurretTransform.rotation, newTurretRotation, speed);
 
-                //поворачиваем турель
-                var speed = turret.RotateSpeed * Time.deltaTime;                
-                turret.TurretTransform.rotation = Quaternion.Lerp(turret.TurretTransform.rotation, newTurretRotation, speed);
+                    BarrelRorartion(aiming.AimPosition, turret, ref newBarrelRotation);  
+                    turret.BarrelTransform.rotation = Quaternion.Lerp(turret.BarrelTransform.rotation, newBarrelRotation, speed);                             
+                }  
+                else
+                {
+                    turret.TurretTransform.rotation = Quaternion.Lerp(turret.TurretTransform.rotation, newTurretRotation, speed);
+                    turret.BarrelTransform.rotation = Quaternion.Lerp(turret.BarrelTransform.rotation, newBarrelRotation, speed);
+                }                     
             }
         }
 
-
-        void TurretRotation(Vector3 direction, ref Quaternion newTurretRotation)
-        {                                                  
+        
+        void TurretRotation(Vector3 aimPosition, TankTurretComponent turret, ref Quaternion newTurretRotation)
+        {            
+            var direction = aimPosition - turret.TurretTransform.position;   
             newTurretRotation = Quaternion.LookRotation(direction);
             var tempRot = newTurretRotation.eulerAngles;
             tempRot.x = 0f;
             newTurretRotation = Quaternion.Euler(tempRot);
+        }
+
+
+        private void BarrelRorartion(Vector3 aimPosition, TankTurretComponent turret, ref Quaternion newBarrelRotation)
+        {
+            var direction = aimPosition - turret.BarrelTransform.position;  
+            var angle = Vector3.Angle(turret.TurretTransform.forward, direction);
+
+            newBarrelRotation = turret.TurretTransform.rotation;
+
+            if (angle < MAX_BARREL_ROTATE)
+            {
+               newBarrelRotation = Quaternion.LookRotation(direction);               
+            } 
         }
 
         #endregion
